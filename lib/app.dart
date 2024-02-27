e74c1fef44497f20da113863c271331f9c9378c6
mac0011 <palakramani>
2024-02-27 17:51:14 +0530

    - menu page done

--- lib/app.dart
+++ lib/app.dart
@@ -31,26 +31,26 @@ class OdhaniApp extends StatelessWidget {
 
   ThemeData appTheme(BuildContext context){
     return ThemeData(
-        appBarTheme: AppBarTheme.of(context).copyWith(
-            centerTitle: true,
-            color: Colors.transparent,
-            systemOverlayStyle: const SystemUiOverlayStyle(
-              statusBarColor: Colors.transparent,
-              statusBarIconBrightness: Brightness.dark,
-            ),
-            foregroundColor: kBlackColor,
-            surfaceTintColor: Colors.transparent,
-            toolbarHeight: kToolbarHeight,
-            titleTextStyle: TextStyle(
-                color: kTextSecondaryColor,
-                fontSize: 20.sp,
-                fontWeight: FontWeight.w700,
-                fontFamily: "NunitoSans"
-            ),
-            shape:  UnderlineInputBorder(
-              borderSide: BorderSide(color: kTextSecondaryColor.withOpacity(0.30)),
-            )
-        ),
+        // appBarTheme: AppBarTheme.of(context).copyWith(
+        //     centerTitle: true,
+        //     color: Colors.transparent,
+        //     systemOverlayStyle: const SystemUiOverlayStyle(
+        //       statusBarColor: Colors.transparent,
+        //       statusBarIconBrightness: Brightness.dark,
+        //     ),
+        //     foregroundColor: kBlackColor,
+        //     surfaceTintColor: Colors.transparent,
+        //     toolbarHeight: kToolbarHeight,
+        //     titleTextStyle: TextStyle(
+        //         color: kTextSecondaryColor,
+        //         fontSize: 20.sp,
+        //         fontWeight: FontWeight.w700,
+        //         fontFamily: "NunitoSans"
+        //     ),
+        //     shape:  UnderlineInputBorder(
+        //       borderSide: BorderSide(color: kTextSecondaryColor.withOpacity(0.30)),
+        //     )
+        // ),
         primarySwatch: primarySwatchColor,
         useMaterial3: true,
         fontFamily: "Outfit"
--- lib/app/menu/view/menu_page.dart
+++ lib/app/menu/view/menu_page.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:odhani_design_sqt/app/menu/domian/menu_model.dart';
 import 'package:odhani_design_sqt/app/tab/domian/tab_model.dart';
 import 'package:odhani_design_sqt/utils/utils.dart';
 class MenuPage extends StatefulWidget {
@@ -13,13 +16,108 @@ class _MenuPageState extends State<MenuPage> {
 
   int selectedIndex = 0;
 
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: kBackground,
-      body: Center(
-        child: Text("Menu"),
+      appBar: AppBar(
+        scrolledUnderElevation: 0,
+        toolbarHeight: kToolbarHeight + 110.sp,
+      flexibleSpace: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.sp),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(25.sp),
+            topRight: Radius.circular(25.sp),
+          ),
+          image: DecorationImage(image: AssetImage("assets/images/graphics/card_back_ground.png"),fit: BoxFit.fill)
+        ),
+        child: SafeArea(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding:  EdgeInsets.symmetric(horizontal: 5.sp),
+                      child: Container(
+                        padding: EdgeInsets.all(14.sp),
+                        decoration: BoxDecoration(
+                          color: Colors.transparent,
+                          borderRadius: BorderRadius.circular(100),
+                          border: Border.all(color: kBorderColor),
+                        ),
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Flexible(
+                              child: Text("Menu",style: CustomTextStyle.mediumFont18Style.copyWith(
+                                  color: kBlackColor
+                              ),),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(14.sp),
+                    decoration: BoxDecoration(
+                      color: Colors.transparent,
+                      borderRadius: BorderRadius.circular(100),
+                      border: Border.all(color: kBorderColor),
+                    ),
+                    child: ImageUtil.iconImageClass.notificationIcon,
+                  ),
+                ],
+              ),
+              SizedBox(height: 15.sp,),
+              Row(
+                children: [
+                  SizedBox(
+                    height: 80.sp,
+                    width: 80.sp,
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(100),
+                        child: Image.asset("assets/images/dummy_img/image_1.png",fit: BoxFit.cover,)),
+                  ),
+                  SizedBox(width: 10.sp,),
+                  Expanded(child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Kajal Patel",style: CustomTextStyle.semiBoldRegularFont20Style,),
+                      Text("Boutique",style: CustomTextStyle.regularFont16Style.copyWith(fontSize: 14.sp),)
+                    ],
+                  ))
+                ],
+              )
+            ],
+          ),
+        ),
       ),
+      ),
+      body: ListView.builder(
+        physics: const ClampingScrollPhysics(),
+        padding: EdgeInsets.symmetric(horizontal: 20.sp,vertical: 20.sp),
+        itemCount: MenuModel.menuList.length,
+        itemBuilder: (context, index) {
+        final menu = MenuModel.menuList[index];
+        return Column(
+          children: [
+            SizedBox(height: 10.sp,),
+            Row(
+              children: [
+                Expanded(child: Text(menu.name ?? "",style: CustomTextStyle.regularFont18Style,)),
+                Icon(Icons.arrow_forward_ios_outlined,color: kBlackColor,size: 18.sp,)
+              ],
+            ),
+            SizedBox(height: 10.sp,),
+            const Divider(color: Color(0xffD9D9D9),)
+          ],
+        );
+      },),
 
     );
   }
